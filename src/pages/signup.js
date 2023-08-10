8fba65a76dcbd1425743cceb9734c31d54564572
Elizabeth <tjeudy.e@gmail.com>
2023-08-10 00:02:47 -0400

    added routes to login and signup pgs

---
+++ src/pages/signup.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+function Signup() {
+	return (
+		<form>
+			<label htmlFor="firstName">First Name</label>
+			<input type="text" placeholder="First name" required />
+			<br />
+			<label htmlFor="lastName">Last Name</label>
+			<input type="text" placeholder="Last name" required />
+			<br />
+			<label htmlFor="email">Email Address</label>
+			<input type="email" placeholder="Enter your email" required />
+			<br />
+			<label htmlFor="password">Password</label>
+			<input type="password" placeholder="Enter your password" required />
+			<button type="submit">
+				<Link href="/dashboard">Create account</Link>
+			</button>
+		</form>
+	);
+}
+
+export default Signup;
